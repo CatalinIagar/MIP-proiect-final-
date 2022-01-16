f45ec973f4d915ad04e5fa05c48bc195d5e1b20f
iosifdaniel07 <iosifdaniel07@yahoo.com>
2022-01-16 15:37:21 +0200

    Test comment

--- Voting System/Voting System/Models/UserModel.cs
+++ Voting System/Voting System/Models/UserModel.cs
@@ -41,6 +41,6 @@ namespace Voting_System.Models {
     }
 
     public class UserDbContext : DbContext {
-        public DbSet <UserModel> Users { get; set; } // cv comment
+        public DbSet <UserModel> Users { get; set; } // cv comment + 7Dani
     }
 }
