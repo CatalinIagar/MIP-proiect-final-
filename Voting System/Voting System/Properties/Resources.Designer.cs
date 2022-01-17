0cc8f55c092290b3b7b57b63317e42c28ff7414d
Gorea Tudor <tudortudor2002@gmail.com>
2022-01-17 17:33:56 +0200

    added UserModel again

--- Voting System/Voting System/Properties/Resources.Designer.cs
+++ Voting System/Voting System/Properties/Resources.Designer.cs
@@ -19,7 +19,7 @@ namespace Voting_System.Properties {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class Resources {
@@ -66,6 +66,16 @@ namespace Voting_System.Properties {
         internal static System.Drawing.Bitmap Election_Polling_Box_Outline_icon_256x256 {
             get {
                 object obj = ResourceManager.GetObject("Election-Polling-Box-Outline-icon_256x256", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap Election_Polling_Box_Outline_icon_256x2562 {
+            get {
+                object obj = ResourceManager.GetObject("Election-Polling-Box-Outline-icon_256x2562", resourceCulture);
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
