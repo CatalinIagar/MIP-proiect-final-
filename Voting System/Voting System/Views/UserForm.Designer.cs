0f1f2f79190e5f072dc655b0ed4d3b307cf51f06
Gorea Tudor <tudortudor2002@gmail.com>
2022-01-19 18:47:30 +0200

    the dgv from UserForm can now be updated

--- Voting System/Voting System/Views/UserForm.Designer.cs
+++ Voting System/Voting System/Views/UserForm.Designer.cs
@@ -23,35 +23,48 @@
         /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent() {
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.dgv = new System.Windows.Forms.DataGridView();
+            this.refreshButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv)).BeginInit();
             this.SuspendLayout();
             // 
-            // dataGridView1
+            // dgv
             // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.RowHeadersWidth = 51;
-            this.dataGridView1.RowTemplate.Height = 24;
-            this.dataGridView1.Size = new System.Drawing.Size(776, 426);
-            this.dataGridView1.TabIndex = 0;
+            this.dgv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv.Location = new System.Drawing.Point(12, 47);
+            this.dgv.Name = "dgv";
+            this.dgv.RowHeadersWidth = 51;
+            this.dgv.RowTemplate.Height = 24;
+            this.dgv.Size = new System.Drawing.Size(776, 391);
+            this.dgv.TabIndex = 0;
+            // 
+            // refreshButton
+            // 
+            this.refreshButton.Location = new System.Drawing.Point(12, 12);
+            this.refreshButton.Name = "refreshButton";
+            this.refreshButton.Size = new System.Drawing.Size(81, 29);
+            this.refreshButton.TabIndex = 1;
+            this.refreshButton.Text = "Refresh";
+            this.refreshButton.UseVisualStyleBackColor = true;
+            this.refreshButton.Click += new System.EventHandler(this.refreshButton_Click);
             // 
             // UserForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.refreshButton);
+            this.Controls.Add(this.dgv);
             this.Name = "UserForm";
             this.Text = "UserForm";
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dgv;
+        private System.Windows.Forms.Button refreshButton;
     }
 }
--- Voting System/Voting System/Views/UserForm.cs
+++ Voting System/Voting System/Views/UserForm.cs
@@ -8,10 +8,32 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+using Voting_System.Models;
+using Voting_System.Controllers;
+
 namespace Voting_System.Views {
     public partial class UserForm : Form {
         public UserForm() {
             InitializeComponent();
+        }
+
+        public async void LoadData () {
+            Task<List<CandidateModel>> task = new Task<List<CandidateModel>>(() => CandidateController.GetAllCandidates());
+            task.Start();
+            List<CandidateModel> candidates = await task; // get all candidates from db
+
+            var res = (from c in candidates
+                       select new {
+                           c.ID,
+                           c.FirstName,
+                           c.LastName,
+                       }).ToList();
+
+            this.dgv.DataSource = res;
+        }
+
+        private void refreshButton_Click(object sender, EventArgs e) {
+            LoadData();
         }
     }
 }
